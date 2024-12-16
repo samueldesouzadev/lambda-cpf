6feb5d47ec44f112300000e4f76c97769cab3869
samueldesouza <samueldesouza.dev@gmail.com>
2024-12-16 08:55:58 -0300

    Initial commit

---
+++ src/main/java/com/validate/lambda/AutenticacaoCPFHandler.java
@@ -0,0 +1,32 @@
+package com.validate.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AutenticacaoCPFHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        String cpf = input.getQueryStringParameters().get("cpf");
+
+        Map<String, String> response = new HashMap<>();
+        if (cpf == null || !cpf.matches("\\d{11}")) {
+            response.put("message", "CPF inválido");
+            return createResponse(400, response);
+        }
+
+        response.put("message", "CPF válido");
+        return createResponse(200, response);
+    }
+
+    private APIGatewayProxyResponseEvent createResponse(int statusCode, Map<String, String> body) {
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.setStatusCode(statusCode);
+        response.setBody(body.toString());
+        return response;
+    }
+}
